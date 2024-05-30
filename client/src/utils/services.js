a2dbda9caf61fbc198e8ad76eb29cacdd0d9988f
Martbul <martbul01@gmail.com>
2024-05-30 23:30:10 +0300

    4 change

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://bookmarks-manager-server.onrender.com";
+export const baseUrl = "https://bookmarks-manager-server.onrender.com/api";
 
 export const postRequest = async (url, body) => {
     const response = await fetch(url, {
