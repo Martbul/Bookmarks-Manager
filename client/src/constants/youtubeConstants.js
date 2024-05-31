02216d6c386c789411b2169cb822ff6cb1644c0d
Martbul <martbul01@gmail.com>
2024-05-31 08:54:06 +0300

    9 change

--- client/src/constants/youtubeConstants.js
+++ client/src/constants/youtubeConstants.js
@@ -1,3 +1,5 @@
 export const YOUTUBE_CLIENT_SECRET='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';
-export const YOUTUBE_REDIRECT_URL = "http://localhost:5173/bookmarks/connections";
+//export const YOUTUBE_REDIRECT_URL = "http://localhost:5173/bookmarks/connections";
+export const YOUTUBE_REDIRECT_URL =
+  "https://bookmarks-manager-pkwm.onrender.com/bookmarks/connections";
 export const YOUTUBE_CLIENT_ID ='514607532464-8e0usq72mockirp1307hgmfv3tjt690k.apps.googleusercontent.com'
